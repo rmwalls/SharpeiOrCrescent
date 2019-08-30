a096979ffcb7b010874459859a8399829d9f7cc5
ZelleM <groovyrlm@gmail.com>
2019-08-30 13:38:00 -0400

    Images shuffle, but you have to click them twice

--- my-app/src/components/display.js
+++ my-app/src/components/display.js
@@ -4,8 +4,9 @@ import data from '../images.json'
 class Display extends Component {
     state = {
         data,
+        clickedIds: [],
         currentScore: 0,
-        topScore: 0
+        status: " "
     }
     // componentDidMount() {
     //     this.setState({
@@ -17,23 +18,56 @@ class Display extends Component {
             <div className="Display" >
                 <header className="App-header" >
                     <h1 className="display-4">Let's Play a Game!</h1>
-                    </header>
-                <div className="jumbotron" >
-                    <p className="lead">Click on the images. Score points by not clicking the same image twice.</p>
-                    <hr />
-                    <h2>Are they sharpeis or crescent rolls?</h2>
-                    <div class="grid-container">
+                    <h2>Click on the images. <br></br>Score points by not clicking the same image twice.</h2>
+                    <p className="label">(Are they sharpeis or crescent rolls?)</p>
+                </header>
+                
+                <div className="gameScore">
+                    <h3 className="score">Your Score =  {this.currentScore}</h3>
+                    <h3 className="status">{this.status}</h3>
+                </div>
+                <div class="grid-container">
                     {
                         this.state.data.map(item => (
                             <div class="grid-item">
-                                <img src={item.image}  alt="game pic" /> 
+                                <img src={item.image} id={this.id} alt="game pic" onClick={() => this.shuffleCard(this.id)} className='shuffleCard' />
                             </div>
                         ))
                     }
-                    </div>
                 </div>
             </div>
         )
     }
+    //shuffle the cards in the browser when clicked
+    shuffleCard = id => {
+        let clickedIds = this.state.clickedIds;
+
+        if (clickedIds.includes(id)) {
+            this.setState({ clickedIds: [], currentScore: 0, status: "Game Over! You lost. Click to play again!" });
+            return;
+        } else {
+            clickedIds.push(id)
+
+            if (clickedIds.length === 12) {
+                this.setState({ score: 12, status: "Wow! You Won! Click to play again!", clickedIds: [] });
+                console.log('Winner');
+                return;
+            }
+            this.setState({ data, clickedIds, currentScore: clickedIds.length, status: " " });
+
+            for (let i = data.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [data[i], data[j]] = [data[j], data[i]];
+            }
+        }
+    }
 }
 export default Display
+
+
+// need to make onClick - handleButtonClick to do the logic
+    //shuffle images
+    //check if clicked before
+    //deal with score
+    // keep going or start over?
+        //play again? 
